7b311ea0d3cadbf03c18a9449d81bb77778c50c4
ismael <ismaelreyplata@gmail.com>
2024-02-28 16:43:48 +0100

    prueba una lista drag and drop

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), DragDropModule]
 };
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CdkDrag,CdkDropList, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop'; 
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [CdkDrag, CdkDropList],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
 export class BodyComponent {
+
+  movies = [
+    'Crear el proyecto de angular',
+    'Crear el componente Header',
+    'Agregar el componente Header al componente principal',
+    'Maquetar el componente Header',
+    'Crear condición sin elegir editar que solo se muestre los datos en el perfil y no pueda modificar los inputs',
+  ];  
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 
 }
