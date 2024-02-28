f5c92d17e5f4f5260756ac1ef665f9d37545e8e2
ismael <ismaelreyplata@gmail.com>
2024-02-28 17:55:26 +0100

    funciona las tres columnas de tareas

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -10,9 +10,34 @@ import { CdkDrag,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem } 
 })
 export class BodyComponent {
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  todo = [
+    "Diseñar la estructura de la aplicación",
+    "Crear el esqueleto HTML",
+    "Estilizar la interfaz de usuario (UI) con CSS",
+    "Implementar la lógica de la aplicación con JavaScript",
+    "Integrar librerías/frameworks necesarios (por ejemplo, React, Vue.js, Angular)",
+    "Optimizar la aplicación para diferentes dispositivos y tamaños de pantalla (responsividad)",
+    "Realizar pruebas de rendimiento y depuración",
+    "Agregar interactividad y animaciones",
+    "Manejar eventos del usuario (clics, teclas, etc.)",
+    "Implementar rutas y navegación (si es una aplicación de una sola página)",
+    "Optimizar la carga de recursos (imágenes, scripts, etc.)",
+    "Asegurar la compatibilidad con diferentes navegadores y versiones",
+    "Agregar accesibilidad para usuarios con discapacidades",
+    "Implementar validación de formularios",
+    "Integrar APIs externas (por ejemplo, APIs REST)",
+    "Diseñar y crear componentes reutilizables",
+    "Documentar el código para futuras referencias",
+    "Realizar pruebas de usabilidad y obtener retroalimentación del usuario",
+    "Preparar la aplicación para producción (minificación, concatenación de archivos, etc.)",
+    "Desplegar la aplicación en un servidor o plataforma de alojamiento",
+    "Configurar herramientas de análisis y monitoreo (por ejemplo, Google Analytics)",
+    "Mantener y actualizar la aplicación según sea necesario"
+  ];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  process : any = [];
+
+  done : any = [];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
