a22b9481003df2552bc15bf8d6a42e6742e0dcda
ismael <ismaelreyplata@gmail.com>
2024-02-29 21:55:09 +0100

    guarda la modificacion de la tarea

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CdkDrag,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'; 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [CdkDrag, CdkDropList],
+  imports: [CdkDrag, CdkDropList, FormsModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
@@ -12,8 +13,15 @@ export class BodyComponent {
 
   @ViewChild('showElementTask') showElementTask!: ElementRef;
 
-  nameTask: any;
+  newArrayList : any = [
+    { id:1 , name:"Nuevo elemento", description: "descripcion del primero elemento" },
+    { id:2 , name:"Segundo nuevo elemento", description: "descripcion del segundo elemento" }
+  ];
+  editTaskSelect: any = [{}];
+  
+  process : any = [];
 
+  done : any = [];
 
   todo = [
     "Diseñar la estructura de la aplicación",
@@ -22,14 +30,9 @@ export class BodyComponent {
     "Implementar la lógica de la aplicación con JavaScript",
   ];
 
-  process : any = [];
+  
 
-  done : any = [];
-
-  newArrayList : any = [
-    { id:1 , name:"Nuevo elemento" },
-    { id:2 , name:"Segundo nuevo elemento" }
-  ];
+  
 
   drop(event: CdkDragDrop<any>) {
     if (event.previousContainer === event.container) {
@@ -44,17 +47,28 @@ export class BodyComponent {
     }
   }
 
-  editTask(nameTask: any){
-    console.log("editando tarea: ",  nameTask);
+  editTask(editTask: any){
+    console.log("editando tarea: ",  editTask);
     this.showElementTask.nativeElement.classList.remove('hideTask');
     this.showElementTask.nativeElement.classList.add('showTask');
-    this.nameTask=nameTask;
+    this.editTaskSelect=editTask;
   }
 
   closeTask(){
     console.log("cerrando tarea");
     this.showElementTask.nativeElement.classList.remove('showTask');
     this.showElementTask.nativeElement.classList.add('hideTask');
+  }
+
+  saveTask(task: any){
+    console.log(task);
+    this.newArrayList.map((e:any)=>{
+      if(e.id == this.editTaskSelect.id ){
+          e.name = task.name;
+          e.description = task.description;
+      }
+    });
+    this.closeTask();
   }
 
 }
