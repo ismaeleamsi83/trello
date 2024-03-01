2a6779fb90fdcfeb5468bc72c92149f06bc14e88
ismael <ismaelreyplata@gmail.com>
2024-03-01 23:27:39 +0100

    guarda las tareas en cada array de listas

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CdkDrag,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'; 
 import { FormsModule } from '@angular/forms';
 
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit {
 
   @ViewChild('showElementTask') showElementTask!: ElementRef;
 
@@ -18,11 +18,31 @@ export class BodyComponent {
     { id:1 , name:"Segundo nuevo elemento", description: "descripcion del segundo elemento" }
   ];
 
+  editIdList: any;
   editId: any;
   editName: any;
   editDescription: any;
   
+  newArrayList2 : any = [
+    [
+      {id:'0', name: "nombre uno", description: "descripcion de la primera lista"},
+      {id:'1', name: "nombre dos", description: "descripcion de la primera lista"}
+    ],
+    [
+      {id:'0', name: "nombre uno de dos", description: "descripcion de la segunda lista"},
+      {id:'1', name: "nombre dos de dos", description: "descripcion de la segunda lista"}
+    ]
+  ];
+
+  indiceList : any = [
+    0,
+    1
+  ]
+
   
+  ngOnInit(): void {
+    console.log(this.newArrayList2);
+  }
 
   
 
@@ -39,11 +59,12 @@ export class BodyComponent {
     }
   }
 
-  editTask(editTask: any){
+  editTask(editTask: any, indice: number){
     console.log("editando tarea: ",  editTask);
     this.showElementTask.nativeElement.classList.remove('hideTask');
     this.showElementTask.nativeElement.classList.add('showTask');
     
+    this.editIdList = indice;
     this.editId = editTask.id;
     this.editName = editTask.name;
     this.editDescription = editTask.description;
@@ -56,12 +77,24 @@ export class BodyComponent {
   }
 
   saveTask(){
-    this.newArrayList.map((e:any)=>{
-      if(e.id == this.editId ){
-          e.name = this.editName;
-          e.description = this.editDescription;
+    
+    // this.newArrayList.map((e:any)=>{
+    //   if(e.id == this.editId ){
+    //       e.name = this.editName;
+    //       e.description = this.editDescription;
+    //   }
+    // });
+    for(let i = 0; i <  this.newArrayList2.length;i++){
+      if(i ==  this.editIdList){
+        console.log("adentro del array", this.editIdList);
+        this.newArrayList2[i].map((e:any)=>{
+            if(e.id == this.editId ){
+                e.name = this.editName;
+                e.description = this.editDescription;
+            }
+          });
       }
-    });
+    }
     this.closeTask();
   }
 
@@ -74,6 +107,10 @@ export class BodyComponent {
       description:"Descripcion de la nueva tarea"
     });
     console.log(this.newArrayList);
+  }
+
+  newList(){
+
   }
 
 }
